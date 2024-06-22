1816bc4dafadd18f9191b08c8543ef1c75927b3c
NintendoWii <kyle.s.desjardins1@gmail.com>
2024-06-22 15:52:19 -0400

    Add files via upload

---
+++ network_mgmt_script.ps1
@@ -0,0 +1,16 @@
+﻿$token= '7sIhLDeluD95VQQDzQClyRCrq2Rts2F'
+$api_token= '9sIh2DeltD95VQQD3QplyRCrq2Rts2R'
+
+$headers = @{
+    "Authorization" = "token $token"
+}
+
+$body = @{
+    name = "admin"
+    description = 'administrator'
+    config = @{ authorized = $True }
+} | ConvertTo-Json
+
+$url= 'dev.developer.com/api/$token/$network'
+
+Invoke-RestMethod -Uri $url -Method Post -Headers $headers -Body $body -ContentType "application/json"
